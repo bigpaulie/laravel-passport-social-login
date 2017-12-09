95a2dc0e80c2232985639aa3a1d0daa988d9d637
Paul P <bigpaulie25ro@yahoo.com>
2017-12-09 20:14:54 +0000

    Fix case sensitive directory name

---
+++ tests/Stubs/helpers.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Define the config helper.
+ */
+if ( !function_exists('config') ) {
+    /**
+     * Mock of config helper.
+     *
+     * @param string $key
+     * @return mixed|null
+     */
+    function config($key)
+    {
+        return Bigpaulie\Laravel\Social\Passport\Tests\Stubs\User::class;
+    }
+}
+
+/**
+ * Define the bcrypt helper.
+ */
+if ( !function_exists('bcrypt') ) {
+    /**
+     * Mock of bcrypt helper.
+     *
+     * @param string $string
+     * @return string
+     */
+    function bcrypt($string)
+    {
+        return md5(time());
+    }
+}
+
