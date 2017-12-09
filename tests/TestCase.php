6b0491cb956fc9993e5a333fdaf076a1c2cc67cf
Paul P <bigpaulie25ro@yahoo.com>
2017-12-09 14:29:22 +0200

    Adding travis configuration

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bigpaulie\Laravel\Social\Passport\Tests;
+
+include 'stubs/helpers.php';
+
+/**
+ * Class TestCase
+ * @package Bigpaulie\Laravel\Social\Passport\Tests
+ */
+class TestCase extends \PHPUnit\Framework\TestCase
+{
+
+}
